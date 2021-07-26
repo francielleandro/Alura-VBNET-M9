11cb4b242c9742662eaa0237069e0ce02d807266
Franciel Leandro <francielleandro@gmail.com>
2021-07-26 06:40:36 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Frm_Principal01.vb
@@ -0,0 +1,43 @@
+﻿Public Class Frm_Principal01
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+        Me.Text = "Banco ByteBank"
+
+
+    End Sub
+
+    Private Sub ContasCorrentesArrayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ContasCorrentesArrayToolStripMenuItem.Click
+        Dim F As New Frm_ContasCorrentesArray
+        F.MdiParent = Me
+        F.Show()
+    End Sub
+
+    Private Sub ContasCorrentesListasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ContasCorrentesListasToolStripMenuItem.Click
+        Dim F As New Frm_ContasCorrentesListas
+        F.MdiParent = Me
+        F.Show()
+    End Sub
+
+    Private Sub ContasCorrentesAgenciaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ContasCorrentesAgenciaToolStripMenuItem.Click
+        Dim F As New Frm_ContasCorrentesAgencia
+        F.MdiParent = Me
+        F.Show()
+    End Sub
+
+    Private Sub ContasCorrentesAgenciaDicToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ContasCorrentesAgenciaDicToolStripMenuItem.Click
+        Dim F As New Frm_ContasCorrentesAgenciaDic
+        F.MdiParent = Me
+        F.Show()
+    End Sub
+
+    Private Sub SimulaçãoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SimulaçãoToolStripMenuItem.Click
+        Dim F As New Frm_Simulacao
+        F.MdiParent = Me
+        F.Show()
+    End Sub
+End Class
